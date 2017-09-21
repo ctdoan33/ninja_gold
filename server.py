7b07d80a6bf660471dca127efa402dca988cb3bc
ctdoan33 <doanchristopher_1@yahoo.com>
2017-09-21 02:21:25 -0700

    dried some code

--- server.py
+++ server.py
@@ -12,29 +12,27 @@ def ninja():
     return render_template("index.html")
 @app.route("/process_money", methods=["POST"])
 def process():
+    timestring=datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)")
     location=request.form["building"]
     if location == "farm":
         delta=random.randint(10, 20)
-        session["gold"]+=delta
-        session["log"].insert(0, ["Earned "+str(delta)+" golds from the farm! "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "green"])
     if location == "cave":
         delta=random.randint(5, 10)
-        session["gold"]+=delta
-        session["log"].insert(0, ["Earned "+str(delta)+" golds from the cave! "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "green"])
     if location == "house":
         delta=random.randint(2, 5)
-        session["gold"]+=delta
-        session["log"].insert(0, ["Earned "+str(delta)+" golds from the house! "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "green"])
     if location == "casino":
         delta=random.randint(-50,50)
-        session["gold"]+=delta
-        if session["gold"] <= 0:
-            session["gold"] = 0
-            session["log"].insert(0, ["Entered a casino and lost all your gold... Ouch... "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "red"])
+        if session["gold"] <= -delta:
+            session["log"].insert(0, ["Entered a casino and lost all your gold... Ouch... "+timestring, "red"])
         elif delta >= 0:
-            session["log"].insert(0, ["Entered a casino and earned "+str(delta)+" golds! "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "green"])
+            session["log"].insert(0, ["Entered a casino and earned "+str(delta)+" golds! "+timestring, "green"])
         else:
-            session["log"].insert(0, ["Entered a casino and lost "+str(-delta)+" golds... Ouch... "+datetime.strftime(datetime.today(), "(%Y/%m/%d %I:%M %p)"), "red"])
+            session["log"].insert(0, ["Entered a casino and lost "+str(-delta)+" golds... Ouch... "+timestring, "red"])
+    else:
+        session["log"].insert(0, ["Earned "+str(delta)+" golds from the "+location+"! "+timestring, "green"])
+    session["gold"]+=delta
+    if session["gold"]<0:
+        session["gold"]=0
     return redirect("/")
 @app.route("/reset", methods=["POST"])
 def reset():
